7ee9e7e28412e618938f6578906737d8a1acf70e
utchi-kanna <118425953+utchi-kanna@users.noreply.github.com>
2023-07-24 19:57:18 +0530

    day11 2

---
+++ src/main/java/day11/practice/TestMysqlConnectionDemo.java
@@ -0,0 +1,10 @@
+package day11.practice;
+
+public class TestMysqlConnectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/day11/practice/UserInsertQuery.java
@@ -0,0 +1,10 @@
+package day11.practice;
+
+public class UserInsertQuery {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
